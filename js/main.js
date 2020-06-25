df1b858a1049e78b9b93cc78be1b3c3b1a5c8ef4
Yuriy Zudikhin <uzudihin@gmail.com>
2020-06-25 15:50:34 +0300

    mobile:

--- js/main.js
+++ js/main.js
@@ -1,12 +1,35 @@
 $(document).ready(function () {
   "use strict";
 
-  $(".grid").masonry({
-    // options
-    itemSelector: ".grid-item",
-  });
+  if ($(window).width() <= 576) {
+    $(".grid").masonry({
+      // options
+      itemSelector: ".grid-item",
+      gutter: 20,
+    });
+  } else {
+    $(".grid").masonry({
+      // options
+      itemSelector: ".grid-item",
+      gutter: 40,
+    });
+  }
 
   $(".test-popup-link").magnificPopup({
     type: "image",
+  });
+
+  $(".grid-item--width1").each(function () {
+    var ratio = 1.5;
+    var width = $(this).width();
+    var result = ratio * width;
+    $(this).css("height", result);
+  });
+
+  $(".grid-item--width2").each(function () {
+    var ratio = 0.665;
+    var width = $(this).width();
+    var result = ratio * width;
+    $(this).css("height", result);
   });
 });
