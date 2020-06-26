bed29fac31f6093a1056bf115984e0c825c72276
Yuriy Zudikhin <uzudihin@gmail.com>
2020-06-26 03:03:50 +0300

    3 a/m

--- js/main.js
+++ js/main.js
@@ -1,35 +1,20 @@
 $(document).ready(function () {
   "use strict";
 
-  if ($(window).width() <= 576) {
-    $(".grid").masonry({
-      // options
-      itemSelector: ".grid-item",
-      gutter: 20,
-    });
-  } else {
-    $(".grid").masonry({
-      // options
-      itemSelector: ".grid-item",
-      gutter: 40,
-    });
-  }
+  $(".tiles-grid").masonry({
+    itemSelector: ".tiles-grid-item",
+    columnWidth: ".tiles-grid-sizer",
+    percentPosition: true,
+    gutter: ".tiles-gutter-sizer",
+  });
+
+  $(".grid").masonry({
+    // options
+    itemSelector: ".grid-item",
+    gutter: ".gutter-sizer",
+  });
 
   $(".test-popup-link").magnificPopup({
     type: "image",
-  });
-
-  $(".grid-item--width1").each(function () {
-    var ratio = 1.5;
-    var width = $(this).width();
-    var result = ratio * width;
-    $(this).css("height", result);
-  });
-
-  $(".grid-item--width2").each(function () {
-    var ratio = 0.665;
-    var width = $(this).width();
-    var result = ratio * width;
-    $(this).css("height", result);
   });
 });
