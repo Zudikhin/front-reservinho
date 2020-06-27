8ae3cb18a6e1ec893b345e29fc4684cd2f85b76e
Yuriy Zudikhin <uzudihin@gmail.com>
2020-06-27 12:29:55 +0300

    background-mage

--- js/main.js
+++ js/main.js
@@ -17,4 +17,26 @@ $(document).ready(function () {
   $(".test-popup-link").magnificPopup({
     type: "image",
   });
+
+  $(".tiles-item").each(function () {
+    if ($(this).attr("data-bg")) {
+      $(this).css({
+        background: "url(" + $(this).data("bg") + ")",
+        "background-repeat": "no-repeat",
+        "background-size": "cover",
+        "background-position": "50%, 50%",
+      });
+    }
+  });
+
+  $(".grid-item").each(function () {
+    if ($(this).attr("data-bg")) {
+      $(this).css({
+        background: "url(" + $(this).data("bg") + ")",
+        "background-repeat": "no-repeat",
+        "background-size": "cover",
+        "background-position": "50%, 50%",
+      });
+    }
+  });
 });
